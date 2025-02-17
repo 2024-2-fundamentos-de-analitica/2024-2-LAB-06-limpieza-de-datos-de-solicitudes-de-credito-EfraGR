0bca93bc0c7d9254e02f918f2f7d4dc01a1cd743
Efra_gR <efrain200414@gmail.com>
2025-02-16 23:23:26 -0500

    envio1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,7 +1,8 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
-
+import pandas as pd
+import os
 
 def pregunta_01():
     """
@@ -13,3 +14,45 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    ruta_entrada = "files/input/solicitudes_de_credito.csv"
+    ruta_salida = "files/output/solicitudes_de_credito.csv"
+    if not os.path.exists("files/output"):
+        os.makedirs("files/output")
+    df_cre = pd.read_csv(ruta_entrada, sep=";", index_col=0)
+    df_cre["sexo"] = df_cre["sexo"].str.strip().str.lower()
+    df_cre["tipo_de_emprendimiento"] = df_cre["tipo_de_emprendimiento"].str.strip().str.lower()
+    df_cre["idea_negocio"] = (
+        df_cre["idea_negocio"]
+        .str.strip().str.lower()
+        .str.replace("á", "a")
+        .str.replace("é", "e")
+        .str.replace("í", "i")
+        .str.replace("ó", "o")
+        .str.replace("ú", "u")
+        .str.replace(" ", "")
+        .str.translate(str.maketrans("", "", "-._"))
+    )
+    df_cre["barrio"] = df_cre["barrio"].str.lower().str.replace("_", " ").str.replace("-", " ")
+    df_cre["comuna_ciudadano"] = df_cre["comuna_ciudadano"].astype(int)
+    df_cre["fecha_de_beneficio"] = pd.to_datetime(df_cre["fecha_de_beneficio"], dayfirst=True, format="mixed")
+    df_cre["monto_del_credito"] = (
+        df_cre["monto_del_credito"]
+        .str.strip()
+        .str.strip("$")
+        .str.replace(".00", "", regex=False)
+        .str.replace(",", "", regex=False).astype(int)
+    )
+    df_cre["línea_credito"] = (
+        df_cre["línea_credito"]
+        .str.strip()
+        .str.lower()
+        .str.replace(" ", "")
+        .str.translate(str.maketrans("", "", "-._"))
+    )
+    
+    df_cre = df_cre.dropna().drop_duplicates()
+    df_cre.to_csv(ruta_salida, index=False, sep=";")
+
+
+
+pregunta_01()
